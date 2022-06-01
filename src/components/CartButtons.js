9a5df89e490744e291a08914048ca5fc0a498423
Rushiraj Ghatge <rushiraj.ghatge@novigosolutions.com>
2022-06-01 12:53:06 +0530

    Added:Cart page to application

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -7,7 +7,8 @@ import { useCartContext } from '../context/cart_context'
 import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
-  const{closeSidebar} = useProductsContext()
+  const{closeSidebar} = useProductsContext();
+  const{total_items} = useCartContext();
   return (
     <Wrapper className='cart-btn-wrapper'>
       <Link to="/cart" className='cart-btn' onClick={closeSidebar}>
@@ -15,7 +16,7 @@ const CartButtons = () => {
         <span className="cart-container">
           <FaShoppingCart />
           <span className="cart-value">
-            10
+            {total_items}
           </span>
         </span>
       </Link>
--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,93 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from "../actions";
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
+    const tempItem = state.cart.find((item) => item.id === id + color); // checking if item is in cart
+    if (tempItem) {
+      // if item is already in the cart
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount;
+          if (newAmount > cartItem.max) {
+            // to check with stock
+            newAmount = cartItem.max;
+          }
+          return { ...cartItem, amount: newAmount };
+        } else {
+          return cartItem;
+        }
+      });
+      return { ...state, cart: tempCart };
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      };
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+  }
+  if (action.type === REMOVE_CART_ITEM) {
+    const tempCart = state.cart.filter((item) => item.id !== action.payload);
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
+  if (action.type === TOGGLE_CART_ITEM_AMOUNT) {
+    const { id, value } = action.payload;
+    const tempCart = state.cart.map((item) => {
+      if (item.id === id) {
+        if (value === "inc") {
+          let newAmount = item.amount + 1; // as we are increasing the quantity
+          if (newAmount > item.max) {
+            // need to check for stock , the increase value should not increase stock value
+            newAmount = item.max;
+          }
+          return { ...item, amount: newAmount };
+        }
+        if (value === "dec") {
+          let newAmount = item.amount - 1; // as we are deccreasing the quantity
+          if (newAmount < 1) {
+            // need to check for stock , the decrease value should not increase stock value
+            newAmount = 1;
+          }
+          return { ...item, amount: newAmount };
+        }
+      } else {
+        return item;
+      }
+    });
+    return { ...state, cart: tempCart };
+  }
 
-export default cart_reducer
+  if (action.type === COUNT_CART_TOTALS) {
+    const { total_items, total_amount } = state.cart.reduce(
+      (total, cartItem) => {
+        const { amount, price } = cartItem;
+        total.total_items += amount;
+        total.total_amount += price * amount;
+
+        return total;
+      },
+      {
+        total_items: 0,
+        total_amount: 0,
+      }
+    );
+    return { ...state, total_items, total_amount };
+  }
+
+  // return state
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default cart_reducer;
